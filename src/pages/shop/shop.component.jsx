bf1db76fc72a1c6400106ae43c1c385d29eec4d8
Jared Gentry <jgentry@kr.af.mil>
2020-05-25 13:02:57 -0400

    Adding images locally; adding a shop page with collection preview and item components

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+import SHOP_DATA from '../../data/shop.data'
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
+const ShopPage = () => {
+  const [collections] = useState(SHOP_DATA)
+
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  )
+}
+
+export default ShopPage
